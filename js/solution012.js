408c83f1c116775363c085687ba03164a20ae34a
edtroit <98837125+edtroit@users.noreply.github.com>
2022-08-02 20:20:56 -0400

    updated solution012

--- js/solution012.js
+++ js/solution012.js
@@ -5,12 +5,12 @@
 //PREP
 //Parameters: Take in a single 'case insensitive string'
 //Return: True if the string contains an equal amount of x's and o's. Zero of each is also an equal amount. Anything other circumstance is false
-// Examples:
-//XO("ooxx") => true
-//XO("xooxx") => false
-//XO("ooxXm") => true
-//XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
-//XO("zzoo") => false
+//Examples:
+//  XO("ooxx") will return true
+//  XO("xooxx") will return false
+//  XO("ooxXm") will return true
+//  XO("zpzpzpp") will return true // when no 'x' and 'o' is present should return true
+//  XO("zzoo") will return false
 //Pseudo Code: Take in string and turn to lowercase to strip case sensitivity. 
 //Create two arrays for x and o and filter by their respective characters. Compare lengths of the two arrays and return result
 
@@ -23,3 +23,8 @@ function XO(str) {
     //check to see if new variables equal each other
     return xArr == yArr
 }
+
+// Cool shorter solution someone else came up with
+//function XO(str) {
+//    return str=str.toLowerCase(), str.split("o").length == str.split("x").length
+//}
