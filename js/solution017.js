a8fa5f1f35bdf410334f9293a5ee30e3d0c31a63
edtroit <98837125+edtroit@users.noreply.github.com>
2022-08-20 17:15:14 -0400

    no new line

--- js/solution017.js
+++ js/solution017.js
@@ -1,4 +1,4 @@
 //https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
 //Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-const even_or_odd = i => i%2 === 0 ? "Even": "Odd";
+const even_or_odd = i => i%2 === 0 ? "Even": "Odd"
