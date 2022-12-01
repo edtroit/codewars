1429c1f3607c938758c548fa8e0c2380a6c851a9
dev account <dev@MacBook-Pro.lan>
2022-11-30 22:48:50 -0500

    second try

--- js/solution044.js
+++ js/solution044.js
@@ -9,6 +9,7 @@ O has an index of 3,
 U has an index of 4.
 Notes: There is no need for string validation and every sentence given will contain all vowels but one. Also, you won't need to worry about capitals.
 
+
 Examples
 "John Doe hs seven red pples under his bsket"          =>  0  ; missing: "a"
 "Bb Smith sent us six neatly arranged range bicycles"  =>  3  ; missing: "o"
